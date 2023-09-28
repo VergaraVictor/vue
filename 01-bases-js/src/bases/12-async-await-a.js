8bbe51127ab22318b5ab1be34879cd597f17000c
VergaraVictor <victorvergara1531@gmail.com>
2023-09-27 21:34:28 -0500

    CreateAsyncAwait

---
+++ 01-bases-js/src/bases/12-async-await-a.js
@@ -0,0 +1,36 @@
+
+const miPromesa = () => {
+    return new Promise( (resolve, reject ) => {
+        setTimeout(() => {
+            //  resolve('Tenemos un valor en la promesa')
+            reject('REJECT en mi promesa')
+        }, 1000);
+    })
+}
+
+
+const medirTiempoAsync = async() =>{
+
+    try {
+        console.log('Inicio')
+
+        const respuesta = await miPromesa()
+        console.log(respuesta)
+
+        console.log('Fin')
+
+         return 'fin de medir tiempo async'
+        
+
+    } catch (error) {
+        // return 'catch en medirTiempoAsync'
+        throw 'Error en medirTiempoAsync'    
+    }
+
+
+    
+}
+
+medirTiempoAsync()
+ .then( valor => console.log( 'THEN Exitoso', valor ))
+ .catch( err => console.log( err ))
--- 01-bases-js/src/index.js
+++ 01-bases-js/src/index.js
@@ -1,28 +1,6 @@
 
-const miPromesa = () => {
-    return new Promise( resolve => {
-        setTimeout(() => {
-             resolve('Tenemos un valor en la promesa')
-        }, 1000);
-    })
-}
-
-
-const medirTiempoAsync = async() =>{
 
 
 
 
-    console.log('Inicio')
-
-    // await miPromesa()
-
-
-    console.log('Fin')
-
-    //return 'fin de medir tiempo async'
-    throw 'Error en medirTiempoAsync'
-}
-
-medirTiempoAsync()
-.then( valor => console.log( valor ))
+console.log('HOla Mundo')
