65808ca1a7c2393fd790bc6e726d4e457c58ab7c
VergaraVictor <victorvergara1531@gmail.com>
2023-09-28 18:01:46 -0500

    ternario

---
+++ 01-bases-js/src/bases/14-ternario.js
@@ -0,0 +1,20 @@
+
+
+
+let firstName;
+let lastName = 'Herrera';
+
+
+// console.log(`${ firstName || 'No firstName' } ${ lastName || 'No lastName'}`)
+
+const isActivate = true
+
+const message = ( isActivate ) ? 'Activo' : 'Inactivo'
+
+// if ( isActivate ) {
+//     message = 'Activo'
+// } else {
+//     message = 'Inactivo'
+// }
+
+console.log(message)
--- 01-bases-js/src/index.js
+++ 01-bases-js/src/index.js
@@ -1,5 +1,5 @@
 
 
 
-console.log('Hola Mundo')
 
+console.log('Hola Mundo')
