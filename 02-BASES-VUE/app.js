e872cd9a51952aeb8e5c976a651e726215ef1219
VergaraVictor <victorvergara1531@gmail.com>
2023-12-05 11:36:46 -0500

    IntroducciónALosEventos

--- 02-BASES-VUE/app.js
+++ 02-BASES-VUE/app.js
@@ -9,6 +9,18 @@ const app = Vue.createApp({
         return{
             quote: "I'm Batman",
             author: 'Bruce Wayne'
+
+        }
+    },
+    methods: {
+        changeQuote() {
+            console.log('Hola Mundo' )
+            this.author = 'Victor Vergara'
+
+            this.capitalize()
+        },
+        capitalize() {
+            this.quote = this.quote.toUpperCase()
         }
     }
 })
