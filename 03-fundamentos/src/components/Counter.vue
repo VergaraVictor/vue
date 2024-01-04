4cfbb3b895d06f52660466380a39cd8f39ef1909
VergaraVictor <victorvergara1531@gmail.com>
2024-01-03 21:18:51 -0500

    PropiedadesComputadas-Computed

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,6 +1,6 @@
 <template>
  <h2>Counter</h2>
- <p> {{ counter }} <sup>2</sup> = {{ counter * counter }} </p>
+ <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
 </template>
 
 <script>
@@ -9,6 +9,16 @@ export default {
     data() {
         return {
             counter: 5
+        }
+    },
+    methods: {
+        getSquareValue() {
+            return this.counter * this.counter
+        }       
+    },
+    computed: {
+        squareCounter() {
+            return this.counter * this.counter
         }
     }
 }
