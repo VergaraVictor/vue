ad2c2b7a788f07518c6c63adc71bb106b0b5c650
VergaraVictor <victorvergara1531@gmail.com>
2024-01-05 16:06:02 -0500

    RealizarPeticiónHTTPaUnBackend

--- 03-fundamentos/src/components/Indecision.vue
+++ 03-fundamentos/src/components/Indecision.vue
@@ -1,5 +1,5 @@
 <template>
-    <img src="https://via.placeholder.com/250" alt="bg">
+    <img v-if="img" :src="img" alt="bg">
     <div class="bg-dark"></div>
 
     <div class="indecision-container">
@@ -12,7 +12,7 @@
 
         <div>
             <h2>{{ question }}</h2>
-            <h1>Si, No, .... pensando</h1>
+            <h1>{{ answer }}</h1>
         </div>
 
     </div>
@@ -22,7 +22,20 @@
 export default {
     data() {
         return {
-            question: null
+            question: null,
+            answer: null,
+            img: null
+        }
+    },
+    methods: {
+        async getAnswer() {
+            this.answer = 'Pensando...'
+
+            const { answer, image } = await fetch('https://yesno.wtf/api').then( r => r.json() )
+            
+            this.answer = answer
+            this.img = image
+
         }
     },
     watch: {
@@ -31,6 +44,7 @@ export default {
             if( !value.includes('?') ) return
 
             // TODO: Realizar petición http
+            this.getAnswer()
         }
     }
 }
