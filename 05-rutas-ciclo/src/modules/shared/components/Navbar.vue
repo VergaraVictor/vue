c9e86413b24e9446ac7dc2b8eac28eca2dd29fbe
VergaraVictor <victorvergara1531@gmail.com>
2024-02-25 22:09:16 -0500

    DispararMetodosDelCicloDeVida

--- 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
+++ 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
@@ -6,16 +6,6 @@
     </div>
 </template>
 
-<script>
-import { defineComponent } from '@vue/composition-api'
-
-export default defineComponent({
-    setup() {
-        
-    },
-})
-</script>
-
 <style scoped>
 
 div {
