2fa258eea01a6c8906d44bda4f24174178f61dec
VergaraVictor <victorvergara1531@gmail.com>
2024-03-24 20:05:16 -0500

    Mutations

--- 06-bases-vuex/src/store/index.js
+++ 06-bases-vuex/src/store/index.js
@@ -1,11 +1,21 @@
 import { createStore } from "vuex"
 
-
 export default createStore({
 
     state: {
         count: 1,
         lastMutation: 'none'
+    },
+    // Son metodos que estas sirven para realizar cambios en el state
+    mutations: {
+        increment( state ) {
+            state.count++
+            state.lastMutation = 'increment'
+        },
+        incrementBy( state, val ) {
+            state.count += val
+            state.lastMutation = 'incrementBy'
+        }
     }
     
 })
