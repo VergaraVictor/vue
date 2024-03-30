f5fca59f10f1458ad162711c7571aa84ed10bdda
VergaraVictor <victorvergara1531@gmail.com>
2024-03-29 20:16:31 -0500

    updateState

--- 07-JOURNAL/src/modules/daybook/store/journal/state.js
+++ 07-JOURNAL/src/modules/daybook/store/journal/state.js
@@ -1,5 +1,24 @@
 
 export default () => ({
-    isLoading: true,
-    entries: [1,2,3,4,5,6,]  // Que el objeto que voy a definir es reactivo 
+    isLoading: true, // Que el objeto que voy a definir es reactivo
+    entries: [
+        {
+            id: new Date().getTime(), //12371298317892
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Adipisicing qui labore esse irure ut adipisicing quis nostrud laboris ex sit. Et laborum ullamco ad Lorem aliquip qui. Non nisi et in sit aliqua exercitation incididunt in.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 1000, //12371298317892
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Laboris officia eiusmod irure non aute duis eu aute dolor quis ipsum. Deserunt cillum laborum velit pariatur ipsum non. Veniam sunt nostrud commodo in consequat irure eiusmod anim cillum cillum ad. In mollit sint proident ut qui cupidatat non culpa nulla ullamco eiusmod ut ea amet. Veniam minim quis qui quis id veniam qui amet. Cupidatat cupidatat enim consectetur laborum aliqua labore adipisicing sint minim. Ut sint eu dolor ullamco deserunt reprehenderit nulla est et Lorem qui reprehenderit.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 2000, //12371298317892
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Nulla cillum proident minim non excepteur aliquip dolore pariatur et magna veniam sunt nisi. Dolor ad non duis nostrud sit deserunt id proident aliquip voluptate reprehenderit in. Mollit voluptate tempor reprehenderit veniam cillum ipsum mollit aute. Incididunt magna tempor ipsum irure fugiat aliquip qui dolor voluptate ipsum labore. Velit aliquip tempor ipsum tempor duis deserunt cillum consequat ad ullamco ullamco in incididunt eu. Aliquip laborum consequat irure qui ad amet dolor sint commodo sunt.',
+            picture: null, // https://
+        }
+    ]   
 })
