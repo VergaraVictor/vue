299940e63189d63db07f079a52d2c31ed1169bac
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-04-25 09:41:05 -0500

    ComprobarQueLasRutasCarguenLosComponentesEsperados

--- 07-JOURNAL/src/modules/daybook/views/EntryView.vue
+++ 07-JOURNAL/src/modules/daybook/views/EntryView.vue
@@ -71,6 +71,7 @@ import getDayMonthYear from '../helpers/getDayMonthYear'
 import uploadImage from '../helpers/uploadImage'
 
 export default {
+    name:'EntryView',
     props: {
         id: {
             type: String,
