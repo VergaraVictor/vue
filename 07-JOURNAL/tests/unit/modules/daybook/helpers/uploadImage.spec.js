164a2fceec5cb9f4295b02e34bee52b608a75cf5
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-04-29 10:52:51 -0500

    Mutation-setEntries

--- 07-JOURNAL/tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ 07-JOURNAL/tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -1,49 +1,42 @@
-describe('Pruebas en la función uploadImage', () => {
-    test('debería cargar un archivo y devolver una URL válida', () => {
-        // Aquí iría el código de la prueba
-    });
-});
+import cloudinary from 'cloudinary'
+import axios from 'axios'
 
-// Este es el codigo de acuerdo fernando pero me genero errores que no me permitio continuar
-// import cloudinary from 'cloudinary'
-// import axios from 'axios'
+import uploadImage from '@/modules/daybook/helpers/uploadImage'
 
-// import uploadImage from '@/modules/daybook/helpers/uploadImage'
-
-// cloudinary.config({
-//     cloud_name: 'ddqwpq9qq',
-//     api_key: '721435814739951',
-//     api_secret: 'm8_VBxnZcresh_fhL0mViUUHFxg'
-// })
+cloudinary.config({
+    cloud_name: 'ddqwpq9qq',
+    api_key: '721435814739951',
+    api_secret: 'm8_VBxnZcresh_fhL0mViUUHFxg'
+})
 
 
-// describe('Pruebas en el uploadImage ', () => {
+describe('Pruebas en el uploadImage ', () => {
     
-//     test('debe de cargar un archivo y retornar el url', async( done ) => {
+    test('debe de cargar un archivo y retornar el url', async( done ) => {
         
-//         const { data } = await axios.get('https://res.cloudinary.com/ddqwpq9qq/image/upload/v1712632643/v2kpzvsgdmoasnlmpzef.jpg', {
-//             responseType: 'arraybuffer'
-//         })
+        const { data } = await axios.get('https://res.cloudinary.com/ddqwpq9qq/image/upload/v1712632643/v2kpzvsgdmoasnlmpzef.jpg', {
+            responseType: 'arraybuffer'
+        })
 
-//         const file = new File([ data ], 'foto.jpg')
+        const file = new File([ data ], 'foto.jpg')
 
-//         const url = await uploadImage( file )
+        const url = await uploadImage( file )
 
-//         expect( typeof url ).toBe('String')
+        expect( typeof url ).toBe('string')
 
 
-//         // Tomar el ID
-//         const segments = url.split('/')
-//         const imageId = segments[ segments.length - 1 ].replace('.jpg','')
-//         cloudinary.v2.api.delete_resources( imageId, {}, () => {
-//             done()
-//         })
+        // Tomar el ID
+        const segments = url.split('/')
+        const imageId = segments[ segments.length - 1 ].replace('.jpg','')
+        cloudinary.v2.api.delete_resources( imageId, {}, () => {
+            done()
+        })
 
         
-//     })
+    })
     
 
-// })
+})
 
 
 
--- 07-JOURNAL/tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ 07-JOURNAL/tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex'
 import journal from '../../../../../../src/modules/daybook/store/journal';
 import state from '../../../../../../src/modules/daybook/store/journal/state';
+import { journalState } from '../../../../mock-data/test-journal-state';
 
 
 
@@ -19,9 +20,28 @@ const createVuexStore = ( initialState ) =>
 describe('Vuex - Pruebas en el Journal module', () => {
 
     // Basicas
-    Test('este es el estado inicial, debe de tener este state', () => {
+    test('este es el estado inicial, debe de tener este state', () => {
 
+        const store = createVuexStore( journalState )
+        const {isLoading, entries } = store.state.journal
+
+        expect(isLoading ).toBeFalsy()
+        expect( entries ).toEqual( journalState.entries )
     })
 
+    // Mutations
+    test('mutation: setEntries', () => {
+
+        const store = createVuexStore({ isLoading: true, entries: [] })
+
+        store.commit('journal/setEntries', journalState.entries )
+        expect( store.state.journal.entries.length ).toBe(2)
+
+        store.commit('journal/setEntries', journalState.entries )
+        expect( store.state.journal.entries.length ).toBe(4)
+        
+        expect( store.state.journal.isLoading ).toBeFalsy()
+
+    })
 
 })
