078485756e854d3c87482240eb1c4710758f832a
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-02 19:48:05 -0500

    CounterUsandoCompositionAPI

--- 08-compo-api/src/views/Counter.vue
+++ 08-compo-api/src/views/Counter.vue
@@ -1,11 +1,39 @@
 <template>
   <h1>Counter View</h1>
+  <h2>{{ counter }}</h2>
+
+  <button @click="increase">+1</button>
+  <button @click="decrease">-1</button>
   
 </template>
 
 <script>
-export default {
+import { ref } from 'vue';
 
+export default {
+  name: 'Counter',
+  props: {},
+  emits: [],
+  setup() {
+
+    const counter = ref(5)
+
+    // const increase = () => {
+    //   counter.value++
+    // }
+
+    // const decrease = () => {
+    //   counter.value--
+    // }
+
+    return {
+      counter,
+
+      decrease: () => counter.value--,
+      increase: () => counter.value++,
+      
+    }
+  }
 }
 </script>
 
