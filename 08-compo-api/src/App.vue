3a4d3d8c3c20b7efc0de241dfb815ebcc9c7a737
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-10 11:52:48 -0500

    RouterKeepAlive

--- 08-compo-api/src/App.vue
+++ 08-compo-api/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
+  <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link :to="{ name: 'counter' }">Counter</router-link>
-  </nav>
-  <router-view/>
+  </div>
+
+
+  <router-view v-slot="{ Component, route }">
+    <keep-alive>
+      <component :is="Component" :key="route.name" />
+    </keep-alive>
+  </router-view>
+
+
 </template>
 
 <style>
