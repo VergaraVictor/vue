189b9e51638a528c138949f1599da91cb1665e9e
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-10 22:26:27 -0500

    LeerParametrosYSegmentosdeURLdesdeElSetup

--- 08-compo-api/src/App.vue
+++ 08-compo-api/src/App.vue
@@ -14,6 +14,7 @@
     </keep-alive>
   </router-view>
 
+  <!-- <router-view /> -->
 
 </template>
 
--- 08-compo-api/src/views/SearchPokemon.vue
+++ 08-compo-api/src/views/SearchPokemon.vue
@@ -4,8 +4,8 @@
         <input
             type="number"
             placeholder="Número del pokémon"
-            autofocus
             v-model="pokemonId"
+            ref="txtSearchId"
         >
     </form>
     <br>
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, onActivated } from 'vue'
 import { useRouter } from 'vue-router'
 
 export default {
@@ -23,9 +23,15 @@ export default {
         const router = useRouter()
         
         const pokemonId = ref(1)
+        const txtSearchId = ref()
+
+        onActivated(() => {
+            txtSearchId.value.select()   
+        })        
         
         return {
             pokemonId,
+            txtSearchId,
 
             onSubmit: () => {
                 // console.log('pokemon a buscar:', pokemonId.value )
