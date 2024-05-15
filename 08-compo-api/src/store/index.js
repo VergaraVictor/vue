88126f3b77c138f9757d4d46985ee8b52aea05bf
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-15 18:36:54 -0500

    VuexGettersCompositionAPI

--- 08-compo-api/src/store/index.js
+++ 08-compo-api/src/store/index.js
@@ -16,6 +16,11 @@ export default createStore({
   },
   actions: {
   },
+  getters: {
+    pendingTodos( state, getters, rootState ) {
+      return state.todos.filter( todo => !todo.completed )
+    }
+  },
   modules: {
   }
 })
--- 08-compo-api/src/views/TodoVuex.vue
+++ 08-compo-api/src/views/TodoVuex.vue
@@ -1,11 +1,27 @@
 <template>
         <h1>Lista de tareas de Thanos</h1>
-        <h4>Pendientes: {{ $store.state.todos.filter( t => !t.completed ).length }}</h4>
+        <!-- <h4>Pendientes: {{ $store.state.todos.filter( t => !t.completed ).length }}</h4> -->
+        <h4>Pendientes: {{ pending.length }}</h4>
+
 </template>
 
 <script>
-export default {
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 
+export default {
+  setup() {
+        
+    const store = useStore()
+
+
+
+    return {
+        pending: computed(() => store.getters['pendingTodos'])
+    }
+
+
+  }
 }
 </script>
 
