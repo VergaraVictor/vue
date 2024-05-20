2ccf6263299f3465c792e440850c2c830785c51d
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-20 08:14:44 -0500

    ManejoDeFormularioyUSeAuthComposable

---
+++ 07-JOURNAL/src/modules/auth/composables/useAuth.js
@@ -0,0 +1,21 @@
+import { useStore } from 'vuex'
+
+
+const useAuth = () => {
+    
+    const store = useStore()
+
+    const createUser = async( user ) => {
+        console.log(user)
+        // TODO: store.dispatch('auth/createUser', user )
+        // return resp
+    }
+
+
+
+    return {
+        createUser
+    }
+}
+
+export default useAuth
