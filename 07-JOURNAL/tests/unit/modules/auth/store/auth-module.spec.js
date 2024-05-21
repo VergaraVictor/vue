825667e5b42ea404e68ba50b99a7a731d4fa89ac
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-21 15:43:14 -0500

    AuthModuleVuexModule

---
+++ 07-JOURNAL/tests/unit/modules/auth/store/auth-module.spec.js
@@ -0,0 +1,25 @@
+
+import createVuexStore from "../../../mock-data/mock-store"
+
+describe('Vuex: Pruebas en el auth-module', () => {
+
+    test('estado inicial', () => {
+
+        const store = createVuexStore({
+            status: 'authenticating', // 'authenticated', 'not-authenticated', 'authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+
+        expect( status ).toBe( 'authenticating' )
+        expect( idToken ).toBe( null )
+        expect( user ).toBe( null )
+        expect( refreshToken ).toBe( null )
+    })
+
+
+
+})
