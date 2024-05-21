c612f0589747c18d126649820c348bc2fa30e743
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-20 19:14:14 -0500

    Interceptores

--- 07-JOURNAL/src/api/journalApi.js
+++ 07-JOURNAL/src/api/journalApi.js
@@ -6,6 +6,19 @@ const journalApi = axios.create({
     baseURL: 'https://vue-demos-9ccc5-default-rtdb.firebaseio.com/'
 })
 
+journalApi.interceptors.request.use( (config) => {
+
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+
+    // config.headers = {
+    //     authorization: 'bearer idToken'
+    // } Esto es comuin verlo pero depende del Backend que se utilice
+
+    return config
+})
+
 // console.log( process.env.NODE_ENV ) // TEST durante testing, 
 
 export default journalApi
