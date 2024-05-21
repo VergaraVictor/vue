c5e490471520557f4f88b6167f27da52d01d665e
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-21 15:53:08 -0500

    MutationLoginUser

--- 07-JOURNAL/tests/unit/modules/auth/store/auth-module.spec.js
+++ 07-JOURNAL/tests/unit/modules/auth/store/auth-module.spec.js
@@ -15,9 +15,36 @@ describe('Vuex: Pruebas en el auth-module', () => {
         const { status, user, idToken, refreshToken } = store.state.auth
 
         expect( status ).toBe( 'authenticating' )
-        expect( idToken ).toBe( null )
         expect( user ).toBe( null )
+        expect( idToken ).toBe( null )
         expect( refreshToken ).toBe( null )
+    })
+
+    // Mutations
+    test('Mutation: loginUser', () => {
+
+        const store = createVuexStore({
+            status: 'authenticating', // 'authenticated', 'not-authenticated', 'authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+
+        const payload = {
+            user: { name: 'victor', email: 'victor@gmail.com' },
+            idToken: 'ABC-123',
+            refreshToken: 'XYZ-123'
+        }
+
+        store.commit('auth/loginUser', payload )
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+
+        expect( status ).toBe( 'authenticated' )
+        expect( user ).toEqual( { name: 'victor', email: 'victor@gmail.com' } )
+        expect( idToken ).toBe( 'ABC-123' )
+        expect( refreshToken ).toBe( 'XYZ-123' )
+
     })
 
 
