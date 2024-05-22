3d26001df43fab715e7bd47bdc8b562b96ed6b6e
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-21 19:08:09 -0500

    ActionsCreateUserErrorUsuarioYaExiste

--- 07-JOURNAL/tests/unit/modules/auth/store/auth-module.spec.js
+++ 07-JOURNAL/tests/unit/modules/auth/store/auth-module.spec.js
@@ -88,8 +88,30 @@ describe('Vuex: Pruebas en el auth-module', () => {
 
     })
 
+
     // Actions
-    
+    test('Actions: createUser - Error usuario ya existe', async() => {
+        
+        const store = createVuexStore({
+            status: 'not-authenticated', // 'authenticated', 'not-authenticated', 'authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+
+        const newUser = { name: 'Test User', email: 'test@test.com', password: '123456' }
+
+        const resp = await store.dispatch('auth/createUser', newUser)
+        expect( resp ).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+
+        expect( status ).toBe( 'not-authenticated' )
+        expect( user ).toBeFalsy()
+        expect( idToken ).toBeFalsy()
+        expect( refreshToken ).toBeFalsy()
+    })
+
 
 
 
