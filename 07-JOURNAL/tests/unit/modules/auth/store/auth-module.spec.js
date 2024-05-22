f3e8e0ed2c2437a69d8c07946f54f6f25d3af596
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-22 12:34:00 -0500

    ActionCheckAuthentication-POSITIVA

--- 07-JOURNAL/tests/unit/modules/auth/store/auth-module.spec.js
+++ 07-JOURNAL/tests/unit/modules/auth/store/auth-module.spec.js
@@ -146,6 +146,32 @@ describe('Vuex: Pruebas en el auth-module', () => {
 
     })
 
+    test('Actions: checkAutehtication - POSITIVA', async() => {
+        
+        const store = createVuexStore({
+            status: 'not-authenticated', // 'authenticated', 'not-authenticated', 'authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
 
+        // SigIn
+        const signInResp = await store.dispatch('auth/signInUser', { email: 'test@test.com', password: '123456' })
+        const { idToken } = store.state.auth
+        store.commit('auth/logout')
+
+        localStorage.setItem('idToken', idToken)
+        
+        const chekResp = await store.dispatch('auth/checkAuthentication')
+        const { status, user, idToken:token, refreshToken } = store.state.auth
+
+        expect(chekResp).toEqual({ ok: true })
+    
+        expect( status ).toBe( 'authenticated' )
+        expect( user ).toMatchObject({ name: 'User Test', email: 'test@test.com' })
+        expect( typeof token ).toBe('string')
+        expect( typeof refreshToken ).toBe('string')
+
+    })
 
 })
