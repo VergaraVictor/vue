ea383944c7137bd0826d596647edb966f39351f6
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-27 16:12:59 -0500

    ComposableUsePlacesStore

--- maps-app/src/store/places/actions.ts
+++ maps-app/src/store/places/actions.ts
@@ -4,10 +4,11 @@ import { StateInterface } from '../index';
 
 
 const actions: ActionTree<PlacesState, StateInterface> = {
+    
     getInitialLocation({ commit }) {
         // TODO: colocar loading
         navigator.geolocation.getCurrentPosition(
-            ({ coords }) => commit('setLngLat', coords),
+            ({ coords }) => commit('setLngLat', {lng: coords.longitude, lat: coords.latitude } ),
             ( err ) => {
                 console.error(err);
                 throw new Error('No geolocation :( ')
