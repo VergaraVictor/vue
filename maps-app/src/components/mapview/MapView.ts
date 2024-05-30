aabd07ede169fe4ab879d1dab08b8d714c233b0c
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-05-30 12:12:35 -0500

    MostrarMapaDeMapbox

--- maps-app/src/components/mapview/MapView.ts
+++ maps-app/src/components/mapview/MapView.ts
@@ -1,5 +1,6 @@
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, ref, toRaw } from "vue";
 import { usePlacesStore } from "@/composables";
+import Mapboxgl from "mapbox-gl";
 
 export default defineComponent({
     name: 'MApView',
@@ -8,8 +9,25 @@ export default defineComponent({
         const mapElement = ref<HTMLDivElement>();
         const { userLocation, isUserlocationReady } = usePlacesStore();
 
+        const initMap = () => {
+            if ( !mapElement.value ) throw new Error('Div Element no exist');
+            if ( !userLocation.value ) throw new Error('user location no existe');
+
+            const map = new Mapboxgl.Map({
+                container: mapElement.value, // container ID
+                style: 'mapbox://styles/mapbox/streets-v12', // style URL
+                center: userLocation.value, // starting position [lng, lat]
+                zoom: 15, // starting zoom
+            });   
+        
+        }
+
         onMounted(() => {
-            console.log( mapElement.value );            
+            if ( isUserlocationReady ) 
+                return initMap();
+            
+            console.log('No tengo localización aun!!!');
+            
         });
 
         return {
--- maps-app/src/main.ts
+++ maps-app/src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import mapboxgl from 'mapbox-gl'; 
+mapboxgl.accessToken = 'pk.eyJ1IjoidmljdG9ydmVyZ2FyYTE1MzEiLCJhIjoiY2x3cjBhZjZuMDdpdTJrb2kzdjZveDAzayJ9.lJyNiERXR4I-1AHC0v0yEw';
 
 if ( !navigator.geolocation ) {
     alert('Tu navegador no soporta el Geolocation');
